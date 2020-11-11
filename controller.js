bcf7754b0ce30a5a66d2c9fd99c79a8f94a42422
kristianmikhael667 <kristianmikhael667@gmail.com>
2020-11-11 13:56:50 +0700

    Good View All Data Mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./rest');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-    response.ok("Aplikasi Rest API berjalan")
+    response.ok("Aplikasi Rest API berjalan", res)
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(err, rows, fileds) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
